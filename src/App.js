42d65a62da74f0a85b694584315e89e9255efc55
Rutuja Asawa <rutuja.asawa@ril.com>
2024-11-07 00:06:18 +0530

    weather app done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css'; 
+import WeatherDisplay from './components/WeatherDisplay';
 
-function App() {
+const WeatherApp = () => {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState('');
+
+  const apiKey = process.env.REACT_APP_OPENWEATHER_API_KEY;
+
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const getWeather = async () => {
+    if (!city) {
+      alert('Please enter a city');
+      return;
+    }
+
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+      const weatherResponse = await axios.get(currentWeatherUrl);
+      const forecastResponse = await axios.get(forecastUrl);
+
+      setWeather({
+        current: weatherResponse.data,
+        hourly: forecastResponse.data.list,
+      });
+      setError('');
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+      setError('Error fetching weather data. Please try again.');
+      setWeather(null);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="weather-container">
+      <h1>Weather Now</h1>
+      <div className="input-container">
+        <input 
+          type="text" 
+          placeholder="Enter city" 
+          value={city} 
+          onChange={handleCityChange}
+        />
+        <button onClick={getWeather}>Get Weather</button>
+      </div>
+      {error && <p className="error-message" style={{ color: 'red' }}>{error}</p>}
+      <WeatherDisplay weather={weather} />
     </div>
   );
-}
+};
 
-export default App;
+export default WeatherApp;
